d5dbcb24dc64b052f87e6bc2ee82fd1542c0622f
Castanix <ethanch007@gmail.com>
2023-02-12 19:39:06 -0500

    Design card set page
    Use cards and accordions for general page design and layout.
    Add form for adding a new card into the set.
    Add button (currently inactive) to start flashcard.
    Add back to home (back to the card set list).
    Separate onClick functions into their own handler function.
    Separate sections of a component with comments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import React from "react";
 import Landing from "./pages/Landing/Landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CardSetPage from "./pages/CardSetPage/CardSetPage";
 
 const App = () => {
 	return (
 		<div className="App">
-			<Landing />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/:cardSetId" element={<CardSetPage />} />
+				</Routes>
+			</BrowserRouter>
+			
 		</div>
 	);
 };
