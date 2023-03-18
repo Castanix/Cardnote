b7eab5c105e13f47dcc20b6dfbb82f5da7710126
Castanix <ethanch007@gmail.com>
2023-03-18 17:54:01 -0400

    Fix CardSetPage tests

--- src/tests/CardSetPage.test.tsx
+++ src/tests/CardSetPage.test.tsx
@@ -8,11 +8,12 @@ import CardSetPage, { CardType } from "../pages/CardSetPage/CardSetPage";
 import { SetupServer, setupServer } from "msw/node";
 import { rest } from "msw";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { CardSetType } from "../components/CardSetList/CardSetList";
 
 let document: HTMLElement;
 let user: UserEvent;
 
-const mockedCardSet: CardType[] = [
+const mockedCards: CardType[] = [
 	{
 		card_id: 1,
 		term: "Term 1",
@@ -32,15 +33,28 @@ const mockedCardSet: CardType[] = [
 		set_id: 1
 	},
 ];
+
+const mockedCardSet: CardSetType[] = [
+	{
+		set_id: 1,
+		name: "Test Set",
+		description: "Test Description",
+		numCards: 3,
+	}
+]
 	
 
 const server: SetupServer = setupServer(
 	rest.get(`${ process.env.REACT_APP_SERVER_URI }/card/allCards/1`, (_, res, ctx) => {
-		return res(ctx.status(200), ctx.json(mockedCardSet));
+		return res(ctx.status(200), ctx.json(mockedCards));
+	}),
+
+	rest.get(`${ process.env.REACT_APP_SERVER_URI }/cardset/oneSet/1`, (_, res, ctx) => {
+		return res(ctx.status(200), ctx.json(mockedCardSet[0].description));
 	}),
 
 	rest.post(`${ process.env.REACT_APP_SERVER_URI }/card/addCard`, (_, res, ctx) => {
-		return res(ctx.status(201), ctx.json({ inserted_id: mockedCardSet.length + 1 }));
+		return res(ctx.status(201), ctx.json({ inserted_id: mockedCards.length + 1 }));
 	}),
 
 	rest.delete(`${ process.env.REACT_APP_SERVER_API }/card/deleteCard`, (_, res, ctx) => {
@@ -101,9 +115,10 @@ describe("page renders", () => {
 
 	test("Render set name and description", () => {
 		const name = screen.getByText(/Test Set/i);
-		// const description = screen.getByText()
+		const description = screen.getByText(/Test Description/i);
 
 		expect(name).toBeInTheDocument();
+		expect(description).toBeInTheDocument();
 	});
 
 	test("Render accordions and buttons", () => {
@@ -234,7 +249,7 @@ describe("page actions", () => {
 		expect(cardSetBody).not.toHaveClass("show");
 	});
     
-	// Note: These tests do not work as the states maintaining the data has been removed in lieu of refetching with react query
+	// NOTE: These tests do not work as the states maintaining the data has been removed in lieu of refetching with react query
 	// test("Delete card", async () => {
 	// 	let list = document.querySelectorAll(".accordion-body")[1];
 	// 	let numCards = list.childElementCount;
@@ -280,57 +295,3 @@ describe("page actions", () => {
 	// 	expect(numCards).toBe(4);
 	// });
 });
-
-// test("Page renders", () => {
-// 	const title = screen.getByText(/Test Set/i);
-// 	const description = screen.getByText(/Description/i);
-// 	const addCard = screen.getAllByText(/Add card/i);
-// 	const cardSet = screen.getByText(/Card set/i);
-// 	const homeButton = screen.getByText(/Back to home/i);
-
-// 	expect(title).toBeInTheDocument();
-// 	expect(description).toBeInTheDocument();
-// 	expect(addCard).toHaveLength(2);
-// 	expect(cardSet).toBeInTheDocument();
-// 	expect(homeButton).toBeInTheDocument();
-// });
-
-// test("Number of cards in set", () => {
-// 	const numCards = document.querySelectorAll(".accordion-body")[1].childElementCount;
-
-// 	expect(numCards).toBe(7);
-// });
-
-// test("Add new card to set", async () => {
-// 	const termInput = screen.getByPlaceholderText(/Enter Term/i);
-// 	const definitionInput = screen.getByPlaceholderText(/Enter Definition/i);
-// 	const addButton = screen.getAllByText(/Add card/i)[1];
-
-// 	await user.type(termInput, "Term");
-// 	await user.type(definitionInput, "Definition");
-
-// 	await user.click(addButton);
-
-// 	const numCards = document.querySelectorAll(".accordion-body")[1].childElementCount;
-
-// 	expect(numCards).toBe(8);
-// });
-
-// test("Accordions open and close", async () => {
-// 	const accordions = document.querySelectorAll(".accordion-button");
-// 	const addCardAccordion = accordions[0];
-// 	const cardSetAccordion = accordions[1];
-
-// 	const accordionBodies = document.querySelectorAll(".accordion-collapse");
-// 	const addCardBody = accordionBodies[0];
-// 	const cardSetBody = accordionBodies[1];
-
-// 	expect(addCardBody).toHaveClass("show");
-// 	expect(cardSetBody).toHaveClass("show");
-
-// 	await user.click(addCardAccordion);
-// 	await user.click(cardSetAccordion);
-
-// 	expect(addCardBody).not.toHaveClass("show");
-// 	expect(cardSetBody).not.toHaveClass("show");
-// });
